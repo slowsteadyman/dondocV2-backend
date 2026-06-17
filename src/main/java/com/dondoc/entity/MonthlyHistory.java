545e2441ad9edc5d533592af8649563f50a8da6c
bacon <baycun22@naver.com>
2026-06-18 01:12:19 +0900

    feat(jpa-record) 54: entity 설정

--- src/main/java/com/dondoc/entity/MonthlyHistory.java
+++ src/main/java/com/dondoc/entity/MonthlyHistory.java
@@ -1,5 +1,10 @@
 package com.dondoc.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +14,22 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "monthly_history")
 public class MonthlyHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "target_month")
     private LocalDate targetMonth;
+
+    @Column(name = "avg_ratio")
     private Float avgRatio;
+
+    @Column(name = "house_level")
     private Integer houseLevel;
 }
--- src/main/java/com/dondoc/entity/Recorde.java
+++ src/main/java/com/dondoc/entity/Recorde.java
@@ -1,5 +1,11 @@
 package com.dondoc.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +18,32 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "records")
 public class Recorde {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "category_id")
     private Long categoryId;
+
+    @Column(name = "amount")
     private Long amount;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "memo")
     private String memo;
+
+    @Column(name = "record_date")
     private LocalDate recordDate;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 }
