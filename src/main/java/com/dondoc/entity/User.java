5f4376a5d940003a34615a582b6f257b40e464aa
byChoi <qpqp0109@gmail.com>
2026-06-17 17:29:22 +0900

    feat:53 - user 도메인에 JPA 도입

--- src/main/java/com/dondoc/entity/User.java
+++ src/main/java/com/dondoc/entity/User.java
@@ -1,25 +1,60 @@
 package com.dondoc.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String userId;
+
+    @Column(nullable = false)
     private String userPassword;
+
+    @Setter
+    @Column(nullable = false)
     private String name;
+
+    @Setter
+    @Column(nullable = false)
     private Integer age;
+
+    @Column(nullable = false)
     private Integer currentPigLevel;
+    @Column(nullable = false)
     private Integer currentHouseLevel;
+    @Column(nullable = false)
     private Integer currentCharacterLevel;
+
+    @Setter
+    @Column(nullable = false)
     private Long monthlyIncome;
+
+    @Setter
+    @Column(nullable = false)
     private Integer targetExpenseRatio;
+
+    @CreatedDate
     private LocalDateTime createdAt;
     private LocalDateTime lastLoginAt;
+
+    public void updateLastLoginTime() {
+        this.lastLoginAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/dondoc/service/AuthService.java
+++ src/main/java/com/dondoc/service/AuthService.java
@@ -31,7 +31,7 @@ public class AuthService {
         if(user == null || !user.getUserPassword().equals(request.getUserPassword())) {
             throw new ApiException(HttpStatus.CONFLICT, "아이디나 비밀번호가 일치하지 않습니다.");
         } else  {
-            userRepository.updateLastLoginAt(user.getId());
+            user.updateLastLoginTime();
             return new LoginResponse(
                     user.getId(),
                     user.getName(),
@@ -48,9 +48,24 @@ public class AuthService {
     @Transactional
     public Long createUser(SignUpRequest request){
         if(userRepository.findByUserId(request.getUserId()).isEmpty()) {
-            Long userId = userRepository.save(request);
-            farmService.addFarmMember(userId, defaultFarmId);
-            return userId;
+            User newUser = new User(
+                    null,
+                    request.getUserId(),
+                    request.getUserPassword(),
+                    request.getName(),
+                    0,
+                    5,
+                    3,
+                    3,
+                    0L,
+                    0,
+                    null,
+                    null
+            );
+
+            User savedUser = userRepository.save(newUser);
+            farmService.addFarmMember(savedUser.getId(), defaultFarmId);
+            return savedUser.getId();
         } else {
             throw new ApiException(HttpStatus.CONFLICT, "이미 사용 중인 아이디입니다.");
         }
