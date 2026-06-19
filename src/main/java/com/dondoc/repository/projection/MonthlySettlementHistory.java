3c00552b95e6a2f0ac9e3b3f5b80e6c677a9bccb
bacon <baycun22@naver.com>
2026-06-19 14:10:35 +0900

    feat(jpa-record) #54: 프로젝션을 위한 class => interface 전환

--- src/main/java/com/dondoc/repository/projection/MonthlySettlementHistory.java
+++ src/main/java/com/dondoc/repository/projection/MonthlySettlementHistory.java
@@ -1,11 +1,6 @@
 package com.dondoc.repository.projection;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class MonthlySettlementHistory {
-    private final Float avgRatio;
-    private final Integer houseLevel;
+public interface MonthlySettlementHistory {
+    Float getAvgRatio();
+    Integer getHouseLevel();
 }
